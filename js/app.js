3b2794b520e953505e0a0e1244af935340b514e2
Tereza <tereza.zizkova366@gmail.com>
2024-11-10 16:46:52 +0100

    funkce na pridavani score po kliknuti

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+// let shotImage = document.getElementById("shotImage")
 
+// shotImage.addEventListener("click", function () {
+    
+//     let scoreElement = this.nextElementSibling
+//         score = Number(scoreElement.textContent)
+
+//     score = score + 1
+//     scoreElement.textContent = score
+
+    
+// })
+
+let shotImage = document.getElementById("shotImage");
+
+
+    shotImage.addEventListener("click", function () {
+        
+        let scoreElement = this.nextElementSibling
+            
+        
+        if (scoreElement && scoreElement.textContent) {
+            let score = Number(scoreElement.textContent)
+            score = score + 1
+            scoreElement.textContent = score
+        }
+    });
