52190a20966ce3b13ad3c7445846b321a71a9a48
Tereza <tereza.zizkova366@gmail.com>
2024-11-10 12:26:12 +0100

    pridani vite config a nastaveni portu

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Tady můžeš přidat specifické nastavení podle potřeby
+  server: {
+    port: 3000, // Určuje port pro vývojový server
+  },
+});
