7c6c3076c74f2b4b1a5bccbf901b9f30f3f188d3
Tereza <tereza.zizkova366@gmail.com>
2024-11-10 13:58:32 +0100

    pridani funkce na presmerovani na game.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 import { gsap } from 'gsap'
 
+const startGame = document.getElementById("startGame")
 
+// presmerovani na stranku game.html
+startGame.addEventListener("click", clickStart)
+
+function clickStart(params) {
+    window.location.href = "game.html"
+}
 
 
 
