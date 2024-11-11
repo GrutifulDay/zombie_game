97552619df04cf65317127ee5ec55133a15f8f90
Tereza <tereza.zizkova366@gmail.com>
2024-11-11 13:35:28 +0100

    uprava json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Tady můžeš přidat specifické nastavení podle potřeby
   server: {
-    port: 3000, // Určuje port pro vývojový server
+    port: 3001, // Určuje port pro vývojový server
   },
 });
