7843539272ecf7407333eeb902b8bff31b7f9f93
Tereza <tereza.zizkova366@gmail.com>
2024-12-09 19:38:36 +0100

    sjednoceni funkce. Po kliknuti na start, pridana fce na odpocet.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,67 @@ import { gsap } from 'gsap'
 
 
 const startGame = document.getElementById("startGame")
+let timeLeft = 3
+const timerElement = document.getElementById("timer")
+
 
 // presmerovani na stranku game.html
 startGame.addEventListener("click", clickStart)
 
-function clickStart(params) {
-    window.location.href = "game.html"
+function clickStart() {
+
+const countDown = setInterval(() => {
+    timeLeft--
+    timerElement.textContent = timeLeft
+
+    if (timeLeft <= 0) {
+        clearInterval(countDown)
+        timerElement.textContent = "jddiii"
+       
+        setTimeout(() => {
+            window.location.href = "game.html"
+        }, 1000)
+    }
+}, 1000)
+      
 }
+
+
+
+
+
+
+
+
+
+
+let timeL = 3
+
+const countDownStart = document.getElementById("countDownStart")
+
+const countD = setInterval(() => {
+    timeL--
+    countDownStart.textContent = timeL
+
+    if (timeLeft <= 0) {
+        clearInterval(countDown)
+        countDownStart.textContent = "GO"
+    }
+}, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
