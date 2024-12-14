2d9896afe6fbc0a6a7b4caf23fe36d4b2b9186f8
Tereza <tereza.zizkova366@gmail.com>
2024-12-14 14:13:04 +0100

    pridani fce shotsDown -1 za sestreleni, kdyz je na 0, konec hry. Nazev fce pro score / clickScore

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 const shotImage = document.getElementById("shotImage")
 let scoreElement = document.getElementById("score")
+let shotsElement = document.getElementById("shots")
 
 // let moveInterval = 3000
+let shots = 10
+
 
 
 
 // SKORE uzivatele po kliknuti na obrazek je pricteno +1 za klik
-shotImage.addEventListener("click", function () {
+shotImage.addEventListener("click", function clickScore () {
     
     if (scoreElement && scoreElement.textContent) {
         let score = Number(scoreElement.textContent)
         score = score + 1
         scoreElement.textContent = score
+    }
+})
+
+// -1 za pocet sestreleni
+shotImage.addEventListener("click", function shotsDown () {
+    
+    if (shotsElement && shotsElement.textContent) {
+        let shots = Number(shotsElement.textContent)
+        shots = shots - 1
+        shotsElement.textContent = shots
+
+        if (shots === 0) {
+            alert("Game over")
+        }
     }
 })
 
@@ -29,17 +46,17 @@ function moveImageToNewPosition() {
 setInterval(moveImageToNewPosition, moveInterval)
 
 
-// function clickToggle() {
+function clickToggle() {
     
-//     shotImage.classList.add("show")
+    shotImage.classList.add("show")
 
     
-//     setTimeout(() => {
-//         shotImage.classList.remove("show")
+    setTimeout(() => {
+        shotImage.classList.remove("show")
 
-//         moveImageToNewPosition()
-//     },1000)
-// }
+        moveImageToNewPosition()
+    },1000)
+}
 
 
 
