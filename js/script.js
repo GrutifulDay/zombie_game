b0701e12b5983b631438ef2d272db5ca90811055
Tereza <tereza.zizkova366@gmail.com>
2024-12-19 19:06:19 +0100

    pridani fce pro zobrazeni obrazku

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 import { gsap } from 'gsap'
 
+// fce pro pridani obrazku do herni plochy
+function mainPicture(src) {
+    const img = document.createElement("img")
+    img.src = src
+    img.classList.add("gamePicture")
+
+    document.getElementById("gameArea").appendChild(img)
+}
+
+mainPicture("image/ter.png")
 
 
 
